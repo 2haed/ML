6fbb99bbdd899fedf60dd12ac9c50fe10c395258
2haed <yurlov.daniil1@yandex.ru>
2022-09-11 09:15:59 +0300

    3gramm model

---
+++ text.py
@@ -0,0 +1,91 @@
+import time
+
+import regex as re
+import numpy as np
+import random
+from collections import Counter
+
+filename = 'data\London Dzhek. Belyy Klyk - BooksCafe.Net.txt'
+
+
+def str_reading(filename: str):
+    try:
+        with open(filename, 'r', encoding='UTF-8') as reading_file:
+            text = reading_file.read()
+            # text = re.sub("[[:punct:]]", '', text.lower())
+            text = re.sub(r"[!?]+$", '', text.lower())
+            text = re.split("[^a-яё]+", text)
+            return text
+    except FileNotFoundError as e:
+        return False, e
+
+def dict(text) -> dict:
+    dict = {}
+    for i in range(len(text) - 2):
+        if text[i] not in dict:
+            dict[text[i]] = [text[i+1] + " " + text[i+2]]
+        else:
+            dict[text[i]].append(text[i+1] + " " + text[i+2])
+    for key, val in dict.items():
+        for k, v in Counter(val).items():
+            val = (k,v)
+            if val in dict[key]:
+                dict[key] = [val]
+            else:
+                dict[key].append(val)
+    for key, val in dict.items():
+        filtered_val = [x for x in val if type(x) != str]
+        val.clear()
+        for x in filtered_val:
+            val.append(x)
+    return dict
+
+
+def generate(dictionary, length) -> str:
+    keys = list(dictionary.keys())
+    variaty_list = []
+    word_list = []
+    generated_str = ''
+    k = random.choice(keys)
+    for i in range(length):
+        for j in range(len(dictionary[k])):
+            variaty_list.append(dictionary[k][j][1])
+            word_list.append(dictionary[k][j][0])
+        generated_phrase = random.choices(word_list, weights=variaty_list, k=1)
+        variaty_list.clear()
+        word_list.clear()
+        generated_str += k + " " if i > 0 and k != generated_str.split()[-1] else " "
+        generated_str += generated_phrase[0]
+        generated_str += ' '
+        k = generated_phrase[0].split()[1] if generated_phrase[0].split()[1] in dictionary else random.choice(keys)
+    splitted_str = generated_str.split()
+    while len(splitted_str) > length:
+        splitted_str.pop()
+    generated_str = ' '.join(splitted_str)
+    print(generated_str)
+
+
+# dictionary = {
+#     'ads': [('ahjdfd asd', 3), ('gsas dddf', 2),],
+#     'asdds': [('saa sdd', 1), ('gs df', 1), ('asas dd', 2)],
+#     'asdfs': [('sag dfgd', 2), ('asd gsdf', 1),],
+#     'gdfs': [('sa sdd', 5), ('ag sdf', 1),]
+# }
+# key = list(dictionary.keys())
+# values = list(dictionary.values())
+# k = random.choice(key)
+# variaty_list = []
+# word_list = []
+# # for val, key in dict.items():
+# #     print(val, key[0][0].split()[random.randint(0,1)])
+# for j in range(len(dictionary[k])):
+#     variaty_list.append(dictionary[k][j][1])
+#     word_list.append(dictionary[k][j][0])
+# print(word_list)
+# print(variaty_list)
+x1 = time.time()
+dictionary = dict(str_reading(filename))
+# print(dictionary)
+generate(dictionary, int(input('Введите длину строки: ')))
+x2 = time.time()
+print(f' Программма сработала за: {x2-x1}')
