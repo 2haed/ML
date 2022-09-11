2eccef5b9c465ced0ca2095dd3eef8aa8fc52129
2haed <yurlov.daniil1@yandex.ru>
2022-09-11 11:57:24 +0300

    ngramm model + 2 file format

--- text.py
+++ text.py
@@ -5,12 +5,12 @@ import numpy as np
 import random
 from collections import Counter
 
-filename = 'Palahniuk Chuck. Fight Club - royallib.ru.txt'
+filename = 'test.txt'
 
 
 def str_reading(filename: str):
     try:
-        with open(filename, 'r', encoding='windows-1251') as reading_file:
+        with open(filename, 'r', encoding='UTF-8') as reading_file:
             text = reading_file.read()
             text = re.sub(r"[!?]+$", '', text.lower())
             text = re.split("[^a-яё]+", text)
@@ -27,29 +27,32 @@ def dict(text, n) -> dict:
         if text[i] not in dict:
             dict[text[i]] = [' '.join(next_words)]
         else:
-            dict[text[i]].append([' '.join(next_words)])
-    print(dict)
-    # for key, val in dict.items():
-    #     for k, v in Counter(val).items():
-    #         val = (k,v)
-    #         if val in dict[key]:
-    #             dict[key] = [val]
-    #         else:
-    #             dict[key].append(val)
-    # for key, val in dict.items():
-    #     filtered_val = [x for x in val if type(x) != str]
-    #     val.clear()
-    #     for x in filtered_val:
-    #         val.append(x)
-    # return dict
+            dict[text[i]].append(' '.join(next_words))
+    for key, val in dict.items():
+        for k, v in Counter(val).items():
+            val = (k,v)
+            if val in dict[key]:
+                dict[key] = [val]
+            else:
+                dict[key].append(val)
+    for key, val in dict.items():
+        filtered_val = [x for x in val if type(x) != str]
+        val.clear()
+        for x in filtered_val:
+            val.append(x)
+    return dict
 
-
-def generate(dictionary, length) -> str:
+def generate(filename, length, n, prefix=None) -> str:
+    dictionary = dict(str_reading(filename), n)
     keys = list(dictionary.keys())
     variaty_list = []
     word_list = []
-    generated_str = ''
-    k = random.choice(keys)
+    if prefix and prefix in dictionary:
+        generated_str = prefix
+        k = prefix
+    else:
+        k = random.choice(keys)
+        generated_str = k
     for i in range(length):
         for j in range(len(dictionary[k])):
             variaty_list.append(dictionary[k][j][1])
@@ -59,35 +62,12 @@ def generate(dictionary, length) -> str:
         word_list.clear()
         generated_str += k + " " if i > 0 and k != generated_str.split()[-1] else " "
         generated_str += generated_phrase[0]
-        generated_str += ' '
-        k = generated_phrase[0].split()[1] if generated_phrase[0].split()[1] in dictionary else random.choice(keys)
-    splitted_str = generated_str.split()
-    splitted_str = splitted_str[:(len(splitted_str)-length)]
-    generated_str = ' '.join(splitted_str)
+        k = generated_phrase[0].split()[n-2] if generated_phrase[0].split()[n-2] in dictionary else random.choice(keys)
+    generated_str = ' '.join(generated_str.split()[:length]).replace("  ", " ")
     print(generated_str)
 
 
-# dictionary = {
-#     'ads': [('ahjdfd asd', 3), ('gsas dddf', 2),],
-#     'asdds': [('saa sdd', 1), ('gs df', 1), ('asas dd', 2)],
-#     'asdfs': [('sag dfgd', 2), ('asd gsdf', 1),],
-#     'gdfs': [('sa sdd', 5), ('ag sdf', 1),]
-# }
-# key = list(dictionary.keys())
-# values = list(dictionary.values())
-# k = random.choice(key)
-# variaty_list = []
-# word_list = []
-# # for val, key in dict.items():
-# #     print(val, key[0][0].split()[random.randint(0,1)])
-# for j in range(len(dictionary[k])):
-#     variaty_list.append(dictionary[k][j][1])
-#     word_list.append(dictionary[k][j][0])
-# print(word_list)
-# print(variaty_list)
 x1 = time.time()
-dictionary = dict(str_reading(filename), 5)
-# print(dictionary)
-# generate(dictionary, int(input('Введите длину строки: ')))
+generate(filename, 10, 5, 'для')
 x2 = time.time()
 print(f' Программма сработала за: {x2-x1}')
--- train.py
+++ train.py
@@ -1,15 +1,93 @@
 import argparse
 import os
-
 from model import BigramModel
+import random
+import pickle
+from typing import Union
+import regex as re
+from collections import Counter
+from dataclasses import dataclass, field
 
+
+@dataclass
+class BigramModel:
+    model_filename: str = field(default="model.pkl")
+    model_weights: dict = field(default_factory=dict)
+
+    def fit(self, filename: str) -> tuple[bool, Union[Exception, None]]:
+        try:
+            with open(filename, 'r', encoding='UTF-8') as reading_file:
+                text = reading_file.read()
+        except FileNotFoundError as e:
+            return False, e
+
+        try:
+            text = re.sub("[[:punct:]]", '', text.lower())
+            text = re.split("[^a-яё]+", text)
+            dict = {}
+            for i in range(len(text) - 1):
+                if text[i] not in dict:
+                    dict[text[i]] = [text[i + 1]]
+                else:
+                    dict[text[i]].append(text[i + 1])
+            for key, val in dict.items():
+                for k, v in Counter(val).items():
+                    val = (k, v)
+                    if val in dict[key]:
+                        dict[key] = [val]
+                    else:
+                        dict[key].append(val)
+            for key, val in dict.items():
+                filtered_val = [x for x in val if type(x) != str]
+                val.clear()
+                for x in filtered_val:
+                    val.append(x)
+            self.model_weights |= dict
+            return True, None
+        except Exception as e:
+            return False, e
+
+    def generate(self, length, prefix=None) -> tuple[bool, Union[Exception, None]]:
+        try:
+            with open(self.model_filename, 'rb') as f:
+                self.model_weights = pickle.load(f)
+        except FileNotFoundError as e:
+            return False, e
+        try:
+            keys = list(self.model_weights.keys())
+            variaty_list = []
+            word_list = []
+            generated_str = prefix + " " if prefix is not None else ''
+            k = random.choice(keys)
+            for _ in range(length - int(prefix is not None)):
+                for j in range(len(self.model_weights[k])):
+                    variaty_list.append(self.model_weights[k][j][1])
+                    word_list.append(self.model_weights[k][j][0])
+                generated_word = random.choices(word_list, weights=variaty_list, k=1)
+                variaty_list.clear()
+                word_list.clear()
+                generated_str += generated_word[0]
+                generated_str += ' '
+                k = generated_word[0]
+            yield generated_str
+            with open('generated.txt', 'w') as f:
+                f.write(generated_str)
+            return True, str
+        except Exception as e:
+            return False, e
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        with open(file=self.model_filename, mode='wb') as file:
+            pickle.dump(self.model_weights, file)
 
 def main():
-    parser = argparse.ArgumentParser(description='Big-ramModel')
+    parser = argparse.ArgumentParser(description='ngramm-Model')
     parser.add_argument("--input-dir", help="Input directory")
-    parser.add_argument("--model", nargs='?', help="JOPA Model")
+    parser.add_argument("--model", nargs='?', help="Model filename")
     args = parser.parse_args()
-
     with BigramModel(model_filename=args.model) as model:
         for filename in os.listdir(args.input_dir):
             processed, err = model.fit(args.input_dir + "/" + filename)
